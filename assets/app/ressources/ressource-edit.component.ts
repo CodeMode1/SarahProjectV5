186acf8a39a44ec58c103adae4b6cd473aabf804
DaphDC <daphnedc21@gmail.com>
2016-11-13 22:11:57 -0500

    ressources4

--- assets/app/ressources/ressource-edit.component.ts
+++ assets/app/ressources/ressource-edit.component.ts
@@ -19,10 +19,12 @@ export class RessourceEditComponent implements OnInit, OnChanges {
     @Output() vider = new EventEmitter<any>();
     etatUserMessage: boolean;
     userMessage: string;
+    activeBoutons: boolean;
 
     constructor(private _ressourceService: RessourceService, private _erreurService: ErreurService) { 
             this.etatUserMessage = false;
             this.estAjout = true;
+            this.activeBoutons = false;
         }
 
     ngOnInit() { 
@@ -46,6 +48,7 @@ export class RessourceEditComponent implements OnInit, OnChanges {
                     data => {
                         this.etatUserMessage = true;
                         this.userMessage = "Ressource Supprimée: " + this.myRessource.nom;
+                        this.myRessource.nom = "";
                         console.log(data);
                     },
                     error => this._erreurService.handleErreur(error)
@@ -58,35 +61,45 @@ export class RessourceEditComponent implements OnInit, OnChanges {
         this.vider.emit(null);
     }
 
+    nomInput(item){
+        if(item.value.length > 0){
+            this.activeBoutons = true;
+        }else{
+            this.activeBoutons = false;
+        }
+    }
+
     onSubmit(ressource: Ressource){
         console.log(ressource);
         // if nouveau, appel créé, sinon appel update
-        if(this.estAjout){
-            console.log("ressource a sauvegarder : ");
-            this.myRessource = ressource;
-            this._ressourceService.creerRessource(this.myRessource)
-                .subscribe(
-                    data => {
-                        this._ressourceService.ressources.push(data);
-                        // message succes creation ressource
-                        this.etatUserMessage = true;
-                        this.userMessage = "Ressource Crée: " + this.myRessource.nom;
-                        this.myRessource.nom = "";
-                    },
-                    error => this._erreurService.handleErreur(error)
-                );
-        } else{
-            this._ressourceService.updateRessource(this.myRessource)
-                .subscribe(
-                    data => {
-                        console.log("edit SUCCES : ");
-                        console.log(data);
-                        this.etatUserMessage = true;
-                        this.userMessage = "Ressource Sauvegardée: " + this.myRessource.nom;
-                        this.viderRessource();
-                    },
-                    error => this._erreurService.handleErreur(error)
-                );
+        if(ressource.nom !== null && ressource.nom !== ""){
+            if(this.estAjout){
+                console.log("ressource a sauvegarder : ");
+                this.myRessource = ressource;
+                this._ressourceService.creerRessource(this.myRessource)
+                    .subscribe(
+                        data => {
+                            this._ressourceService.ressources.push(data);
+                            // message succes creation ressource
+                            this.etatUserMessage = true;
+                            this.userMessage = "Ressource Crée: " + this.myRessource.nom;
+                            this.myRessource.nom = "";
+                        },
+                        error => this._erreurService.handleErreur(error)
+                    );
+            } else{
+                this._ressourceService.updateRessource(this.myRessource)
+                    .subscribe(
+                        data => {
+                            console.log("edit SUCCES : ");
+                            console.log(data);
+                            this.etatUserMessage = true;
+                            this.userMessage = "Ressource Sauvegardée: " + this.myRessource.nom;
+                            this.viderRessource();
+                        },
+                        error => this._erreurService.handleErreur(error)
+                    );
+            }
         }
     }
 }
